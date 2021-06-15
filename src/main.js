57b27c7b623d84d0e4ade66e1e1c9eaa97be19d2
献丑 <chaojie2025@163.com>
2021-06-15 22:21:23 +0800

    add createComponents

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './routers'
+import store from './stores'
+import axios from 'axios'
 
+Vue.prototype.axios = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/stores/index.js
+++ src/stores/index.js
@@ -12,5 +12,7 @@ export default new Vuex.Store({
   },
   actions: {
 
+  },
+  modules : {
   }
 })
